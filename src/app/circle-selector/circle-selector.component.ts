cbae38747676a7c9ad1891df12716c44740aff14
Sergio <webdev.slara@gmail.com>
2022-05-20 14:26:40 +0200

    finished circleselector component, added some comments

--- src/app/circle-selector/circle-selector.component.ts
+++ src/app/circle-selector/circle-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChildren, AfterViewInit } from '@angular/core';
 import { generalStyle } from './general-style-interface';
 
 @Component({
@@ -6,69 +6,69 @@ import { generalStyle } from './general-style-interface';
   templateUrl: './circle-selector.component.html',
   styleUrls: ['./circle-selector.component.css']
 })
-export class CircleSelectorComponent implements OnInit {
+export class CircleSelectorComponent implements OnInit, AfterViewInit  {
 
+  //Changes the title of the circle selector
   @Input() title: string;
+  //Modifies the list of items you want to display
   @Input() numberList: (string | number)[] = []
+  //Sets up default clicked value
   @Input() defaultValue: number | string
+  //Sets up the style of the circle selector component (title and circles)
   @Input() titleStyle: generalStyle = {}
   @Input() circleStyle: generalStyle = {}
-  @Input() titleStyleActive: generalStyle = {}
-  @Input() circleStyleActive: generalStyle = {}
 
   constructor() {
     this.title = 'defaultTitle'
     this.numberList = [1, 2, 3, 4, '5+']
     this.defaultValue = 2
-    this.circleStyle = {
-      fontFamily: 'Arial',
-      width: '60px',
-      height: `60px`,
-      fontSize: '30px',
-      color: '#FFFFFF',
-      backgroundColor: '#18acb5',
-      margin: '0.20em',
-    },
     this.titleStyle = {
       fontFamily: 'Arial',
       fontSize: '1.25em',
       color: 'black'
     },
-    this.titleStyleActive = {},
-    this.circleStyleActive = {
-      backgroundColor: '#f0f18f',
-      color: '#94979b'
+    this.circleStyle = {
+      backgroundColor: '#f0f1f8',
+      color: '#94979b',
+      fontFamily: 'Arial',
+      width: '60px',
+      height: `60px`,
+      fontSize: '30px',
+      margin: '0.20em'
     }
   }
 
-  ngOnInit(): void {
-  } 
+  @ViewChildren('circle') circle!: ElementRef | any;
 
-  @ViewChild('circle') circle!: ElementRef | any;
+  ngOnInit(): void {} 
 
-  onClick($event: Event): void {
-    ($event.target as HTMLElement).classList.toggle('active')
-    let circlesContainer = document.querySelectorAll('.circle-selector-circle')
-    circlesContainer.forEach(circle => {
-      circle.classList.contains('active')
+  ngAfterViewInit(): void {
+    let circleHighlighted = this.circle._results[this.numberList.indexOf(this.defaultValue)]
+    circleHighlighted.nativeElement.style.setProperty('background-color','#18acb5')
+    circleHighlighted.nativeElement.style.setProperty('color','#FFFFFF')
+  }
+
+  onClick($event: Event): void{
+    let elem = ($event.target as HTMLElement)
+    let index = this.numberList.map( num => num.toString()).indexOf(elem.innerText)
+    this.circle._results.forEach((elem: ElementRef, i: number) => {
+      if(index === i){
+        this.setClicked(elem)
+        return
+      }
+      this.setUnclicked(elem)
     })
+  }
 
-    //elem.target.classList.toggle('active')
+  setUnclicked(elem: ElementRef): void {
+    elem.nativeElement.style.setProperty('background-color','#f0f1f8')
+    elem.nativeElement.style.setProperty('color','#94979b')
+  }
 
+  setClicked(elem: ElementRef): void {
     
-   
-  }
-
-  setStyles(circle: HTMLElement){
-    this.isActive(circle) 
-    ? circle.style.backgroundColor = '#18acb5'
-    : circle.style.backgroundColor = '#f0f1f8'
-  }
-
-  isActive(circle: any): boolean{
-    return circle.nativeElement.classList.contains('active')
-      ? true
-      : false
+    elem.nativeElement.style.setProperty('background-color','#18acb5')
+    elem.nativeElement.style.setProperty('color','#FFFFFF')
   }
 
 }

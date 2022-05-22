8263261e02ea8ecdc38b31a161dd43d82f66b46f
Sergio <webdev.slara@gmail.com>
2022-05-22 18:44:47 +0200

    size adapts now

--- src/app/navigation-line/navigation-line.component.ts
+++ src/app/navigation-line/navigation-line.component.ts
@@ -9,11 +9,47 @@ import { faCheck, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 })
 export class NavigationLineComponent implements OnInit {
 
+  //list of element to display
   @Input() elemList : (string | number)[]
+
+  //icon to place inside the navigation line
   @Input() symbol : IconDefinition
+
+  //size of the circles of the navigation line
+  @Input() circleWidth: number;
+  @Input() circleHeight: number;
+
+  //size of the lines between circles
+  @Input() lineWidth: number;
+  @Input() lineHeight: number
+
+  @Input() lineStyle = {
+    circle: {},
+    line: {}
+  }
+  @Input() listStyle = {}
+
   constructor() {
     this.elemList = ['Datos personales', 'Situación actual', 'Stituación económica', 'Oferta', 'Identificación', 'Firma y documentación']
     this.symbol = faCheck
+    this.circleWidth = 30
+    this.circleHeight = 30
+    this.lineWidth = 4
+    this.lineHeight = 50
+    this.listStyle = {
+      fontFamily: 'Arial',
+    },
+    this.lineStyle = {
+      circle: {
+        width: `${this.circleWidth}px`,
+        height: `${this.circleHeight}px`,
+      },
+      line: {
+        width: `${this.lineWidth}px`,
+        height: `${this.lineHeight}px`,
+        left: `${ (this.circleWidth / 2) - (this.lineWidth / 2) }px`,
+      }
+    }
    }
 
   ngOnInit(): void {

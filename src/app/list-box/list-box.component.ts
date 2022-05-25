60dd1e6a7e65d1e9895b78c362d2847c24d0ecb6
Sergio <webdev.slara@gmail.com>
2022-05-25 13:33:27 +0200

    trying to put a triangle instead of the default arrow in the select

--- src/app/list-box/list-box.component.ts
+++ src/app/list-box/list-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-box',
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListBoxComponent implements OnInit {
 
-  constructor() { }
+  @Input() list: string[]
+
+  constructor() {
+    this.list = [
+      'Autónomo',
+      'Asalariado',
+      'Funcionario',
+      'Pensionista'
+    ]
+   }
 
   ngOnInit(): void {
   }

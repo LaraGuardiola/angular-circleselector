bb44fc3ede083a3e8e0d4762c3669c5e5875319e
Sergio <webdev.slara@gmail.com>
2022-05-25 18:15:54 +0200

    icon button style done, now need to be wired up to js

--- src/app/icon-button/icon-button.component.ts
+++ src/app/icon-button/icon-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { faSliders, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-icon-button',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IconButtonComponent implements OnInit {
 
-  constructor() { }
+  //style of the symbol
+  @Input() symbol: IconDefinition
+
+  //style of the text
+  @Input() buttonText: string
+
+  constructor() { 
+    this.symbol = faSliders
+    this.buttonText = 'VER DETALLES'
+  }
 
   ngOnInit(): void {
   }

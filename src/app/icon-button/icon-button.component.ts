0d72dbbaf58f342bfe093085880664f8233b80c5
Sergio <webdev.slara@gmail.com>
2022-05-25 19:18:13 +0200

    icon button completely wired up

--- src/app/icon-button/icon-button.component.ts
+++ src/app/icon-button/icon-button.component.ts
@@ -11,27 +11,41 @@ export class IconButtonComponent implements OnInit {
   //style of the symbol
   @Input() symbol: IconDefinition
   @Input() symbolSize: string
+  @Input() symbolColor: string
 
   //style of the text
   @Input() buttonText: string
+  @Input() buttonColor: string
 
   //style of the fake button
   @Input() hasBorder: boolean
   @Input() borderSize: string
   @Input() borderColor: string
-
+  @Input() width: number
+  @Input() height: number
+  @Input() padding: string
+  @Input() backgroundColor: string
+  @Input() borderRadius: string
 
   constructor() { 
     this.symbol = faSliders
     this.symbolSize = '24px'
+    this.symbolColor = '#18acb5'
     this.buttonText = 'VER DETALLES'
-    this.borderColor = 'red'
+    this.buttonColor = '#18acb5'
+    this.borderColor = '#18acb5'
     this.borderSize = '2px'
     this.hasBorder = true
+    this.width = Math.round(this.buttonText.length * 13.3)
+    this.height = 30
+    this.padding = '0.8em'
+    this.backgroundColor = '#ffffff'
+    this.borderRadius = '5px'
   }
 
   @ViewChild('icon') icon: ElementRef | any
   @ViewChild('button') button: ElementRef | any
+  @ViewChild('text') text: ElementRef | any
 
   ngOnInit(): void {}
 
@@ -39,17 +53,30 @@ export class IconButtonComponent implements OnInit {
     this.setDefaultState()
   }
 
+  //setting up default state
+
   setDefaultState(): void {
     this.setSymbolDefaultState()
     this.setButtonDefaultState()
+    this.setTextDefaultState()
   }
 
   setSymbolDefaultState(): void {
     this.icon.nativeElement.style.setProperty('font-size', this.symbolSize)
+    this.icon.nativeElement.style.setProperty('color', this.symbolColor)
+  }
+
+  setTextDefaultState(): void {
+    this.text.nativeElement.style.setProperty('color', this.buttonColor)
   }
 
   setButtonDefaultState(): void {
     this.button.nativeElement.style.setProperty('border', this.hasBorder ? `${this.borderSize} solid ${this.borderColor}` : 'none')
+    this.button.nativeElement.style.setProperty('width', `${this.width}px`)
+    this.button.nativeElement.style.setProperty('height', `${this.height}px`)
+    this.button.nativeElement.style.setProperty('padding', this.padding)
+    this.button.nativeElement.style.setProperty('background-color', this.backgroundColor)
+    this.button.nativeElement.style.setProperty('border-radius', this.borderRadius)
   }
 
   //*Events
@@ -58,5 +85,4 @@ export class IconButtonComponent implements OnInit {
     let button = (event.target as HTMLElement)
     console.log(button)
   }
-
 }

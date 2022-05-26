279142a7ff17a0c0bc3f3740bc5a2b26285a903b
Sergio <webdev.slara@gmail.com>
2022-05-26 21:49:12 +0200

    added margin for the ng-container, radio button tile almost completely styled

--- src/app/radio-button/radio-button.component.ts
+++ src/app/radio-button/radio-button.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faCheck, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { faCheck, faSkull, faHelicopter, faVolcano, faVirusCovid, faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-radio-button',
@@ -9,19 +8,79 @@ import { faCheck, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 })
 export class RadioButtonComponent implements OnInit {
 
-  @Input() symbol: IconDefinition | any
+  @Input() symbol: any[]
+  @Input() symbol1: any[]
+
   @Input() list: string[]
+  @Input() mainText: string[]
+
+  @Input() isRecommended: boolean
+  @Input() isRecommended1: boolean
+  @Input() isRecommendedGroup: boolean[]
+  @Input() displayLists: boolean[]
+  @Input() recommendedText: string
+
+  @Input() list1: string[]
+  @Input() recommendedText1: string
+
+  @Input() numOfRadioButtons: number
+  @Input() itemList: any[]
 
   constructor() {
-    this.symbol = faCheck
+    this.symbol = [faTrophy, faSkull, faVirusCovid]
+    this.symbol1 = [faVolcano, faSkull]
+
+    this.mainText = [
+      'Sí, cumplo con los requisitos y quiero una respuesta inmediata',
+      'No, no quiero beneficiarme de las ventajas de BankReady'
+    ]
+    
+    this.recommendedText = "Recomendado"
+    this.isRecommended = true
+    this.isRecommended1 = true
+    this.isRecommendedGroup = [this.isRecommended, this.isRecommended1]
+    this.displayLists = [true, true]
+
+    this.recommendedText1 = "No Recomendado"
+
+
+    this.numOfRadioButtons = 2
+
     this.list = [
-      "Envio de tus fondos antes",
-      "Formulario corto",
-      "Menos documentación"
+        "Envio de tus fondos antes",
+        "Formulario corto",
+        "Menos documentación"
+    ],
+    this.list1 = [
+        "Puede que salga",
+        "Puede que no",
+    ]
+    this.itemList = [ 
+      {
+        mainText: this.mainText,
+        isRecommended: this.isRecommendedGroup,
+        displayLists: this.displayLists,
+        list: this.list,
+        symbol: this.symbol
+      },
+      {
+        mainText: this.mainText,
+        isRecommended: this.isRecommendedGroup,
+        displayLists: this.displayLists,
+        list: this.list1,
+        symbol: this.symbol1
+      }
     ]
    }
 
-  ngOnInit(): void {
+  @ViewChild('lista') lista: ElementRef | any
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    
   }
+
+
 
 }

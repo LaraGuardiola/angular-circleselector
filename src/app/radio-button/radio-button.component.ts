2256c98d93b5aabcb66747212002f75938d66f51
Sergio <webdev.slara@gmail.com>
2022-05-26 22:38:40 +0200

    radio button tile done, without js integration this time, mostly done with css and the html template injected by the properties

--- src/app/radio-button/radio-button.component.ts
+++ src/app/radio-button/radio-button.component.ts
@@ -11,22 +11,25 @@ export class RadioButtonComponent implements OnInit {
   @Input() symbol: any[]
   @Input() symbol1: any[]
 
-  @Input() list: string[]
   @Input() mainText: string[]
 
-  @Input() isRecommended: boolean
-  @Input() isRecommended1: boolean
   @Input() isRecommendedGroup: boolean[]
   @Input() displayLists: boolean[]
+  @Input() radioValues: boolean[]
   @Input() recommendedText: string
 
+  @Input() list: string[]
   @Input() list1: string[]
-  @Input() recommendedText1: string
 
-  @Input() numOfRadioButtons: number
   @Input() itemList: any[]
 
   constructor() {
+    this.recommendedText = "Recomendado"
+
+    this.isRecommendedGroup = [true, false]
+    this.displayLists = [true, false]
+    this.radioValues = [true, false]
+
     this.symbol = [faTrophy, faSkull, faVirusCovid]
     this.symbol1 = [faVolcano, faSkull]
 
@@ -34,17 +37,6 @@ export class RadioButtonComponent implements OnInit {
       'Sí, cumplo con los requisitos y quiero una respuesta inmediata',
       'No, no quiero beneficiarme de las ventajas de BankReady'
     ]
-    
-    this.recommendedText = "Recomendado"
-    this.isRecommended = true
-    this.isRecommended1 = true
-    this.isRecommendedGroup = [this.isRecommended, this.isRecommended1]
-    this.displayLists = [true, true]
-
-    this.recommendedText1 = "No Recomendado"
-
-
-    this.numOfRadioButtons = 2
 
     this.list = [
         "Envio de tus fondos antes",
@@ -55,32 +47,37 @@ export class RadioButtonComponent implements OnInit {
         "Puede que salga",
         "Puede que no",
     ]
+
     this.itemList = [ 
       {
         mainText: this.mainText,
         isRecommended: this.isRecommendedGroup,
         displayLists: this.displayLists,
         list: this.list,
-        symbol: this.symbol
+        symbol: this.symbol,
+        radioValues: this.radioValues
       },
       {
         mainText: this.mainText,
         isRecommended: this.isRecommendedGroup,
         displayLists: this.displayLists,
         list: this.list1,
-        symbol: this.symbol1
+        symbol: this.symbol1,
+        radioValues: this.radioValues
       }
     ]
    }
 
-  @ViewChild('lista') lista: ElementRef | any
-
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    
+  ngOnInit(): void {
+    if(typeof this.radioValues[1] === 'boolean'){
+      console.log('es booleano')
+    }else{
+      console.log('no es booleano')
+    }
   }
 
-
-
+  onCheck(event: Event): void {
+    let target = (event.target as HTMLInputElement)
+    console.log(target.value)
+  }
 }

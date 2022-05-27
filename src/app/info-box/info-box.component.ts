bdfec33a8c221900a174c64096ef23aa371741c7
Sergio <webdev.slara@gmail.com>
2022-05-27 10:32:36 +0200

    added bypass for dom sanitizer in info box

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -9,9 +9,13 @@ import { faCommentDollar, IconDefinition } from '@fortawesome/free-solid-svg-ico
 export class InfoBoxComponent implements OnInit {
 
   @Input() icon: IconDefinition
+  @Input() firstText: string
+  @Input() secondText: string
 
   constructor() { 
     this.icon = faCommentDollar
+    this.firstText = '¿Sabes por qué nuestros clientes eligen BankReady?'
+    this.secondText = '<u>Te lo explicamos aquí abajo</u>'
   }
 
   ngOnInit(): void {
